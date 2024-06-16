8eeed0f74d0774e38a2a824197334f6dc109fcb7
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-06-16 19:16:58 +0200

    [PATCH] Temporary patch for the type  issue on hardhat

--- Week3/tokenizedBallot/test/tokenizedBallot.ts
+++ Week3/tokenizedBallot/test/tokenizedBallot.ts
@@ -3,6 +3,13 @@ import { toHex, hexToString } from "viem";
 import { viem, run as hardhatRun } from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 
+import { GetContractReturnType } from "@nomicfoundation/hardhat-viem/types";
+import { ArtifactsMap } from "hardhat/types/artifacts";
+
+type TokenizedBallot = GetContractReturnType<
+  ArtifactsMap["TokenizedBallot"]["abi"]
+>;
+
 before(async function () {
   await hardhatRun("compile");
 });
@@ -20,11 +27,14 @@ async function deployContract() {
     fifthAccount,
   ] = await viem.getWalletClients();
   const tokenContract = await viem.deployContract("MyToken");
-  const tokenizedBallotContract = await viem.deployContract("TokenizedBallot", [
-    PROPOSALS.map((prop) => toHex(prop, { size: 32 })),
-    tokenContract.address,
-    0,
-  ]);
+  const tokenizedBallotContract = (await viem.deployContract(
+    "TokenizedBallot",
+    [
+      PROPOSALS.map((prop) => toHex(prop, { size: 32 })),
+      deployer.account.address,
+      0,
+    ]
+  )) as any as TokenizedBallot;
   return {
     publicClient,
     deployer,
@@ -42,9 +52,9 @@ describe("TokenizedBallot", async () => {
     it("has the provided proposals", async () => {
       const { tokenizedBallotContract } = await loadFixture(deployContract);
       for (let index = 0; index < PROPOSALS.length; index++) {
-        const proposal = (await tokenizedBallotContract.read.proposals([
+        const proposal = await tokenizedBallotContract.read.proposals([
           BigInt(index),
-        ])) as `0x${string}`[];
+        ]);
         expect(hexToString(proposal[0], { size: 32 })).to.eq(PROPOSALS[index]);
       }
     });
