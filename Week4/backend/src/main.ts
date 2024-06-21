c4647d360577b6041b4c5f2d234320da82528a36
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-06-21 02:51:08 +0200

    [FIX] Swagger

--- Week4/backend/src/main.ts
+++ Week4/backend/src/main.ts
@@ -4,7 +4,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
   const config = new DocumentBuilder()
   .setTitle('API example')
   .setDescription('The API description')
@@ -13,6 +12,6 @@ async function bootstrap() {
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api', app, document);
-
+await app.listen(3001);
 }
 bootstrap();
