ebb313dbacb6e1ca958d9572ac86652fa622e508
Ayodeji Komolafe <kvngeko@icloud.com>
2024-06-24 05:05:34 -0400

    added backend changes

--- Week4/backend/src/main.ts
+++ Week4/backend/src/main.ts
@@ -1,18 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-  .setTitle('API example')
-  .setDescription('The API description')
-  .setVersion('1.0')
-  .addTag('example')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-app.enableCors();
-await app.listen(3001);
+    .setTitle('API example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('example')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+  await app.listen(3001);
 }
 bootstrap();
