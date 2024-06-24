dc51fb23199270aad0214a018e68188fbf1caaa2
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-06-24 21:32:12 +0300

    [ADD]: Hardcode the smart contract address for cast votes also and  add a button

--- Week4/frontend/packages/nextjs/components/TokenizedBallot.tsx
+++ Week4/frontend/packages/nextjs/components/TokenizedBallot.tsx
@@ -7,7 +7,8 @@ export function TokenizedBallot() {
 
 
     return (
-      <div className="card flex  w-[80%] p-4   text-primary-content mt-4 gap-4 justify-center items-center">
+      <div className="card flex  w-[100%] p-4    text-primary-content mt-4 gap-4 justify-center items-center">
+        <h1 className="bg-primary font-bold text-4xl rounded-3xl p-4 text-cyan-300">Tokenized Ballot</h1>
         <CastVote/>
         <Delegate/> 
         <Query/>
