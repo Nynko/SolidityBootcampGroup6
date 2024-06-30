06417a5956e0eb7d2da13062995fedaf36c3531d
Nicolas Beaudouin <pro@nicolasbeaudouin.com>
2024-06-30 11:41:23 +0200

    [FIX] Error

--- Week5/frontend/packages/nextjs/app/lottery/page.tsx
+++ Week5/frontend/packages/nextjs/app/lottery/page.tsx
@@ -10,7 +10,7 @@ import { RedeemTokens } from "./components/redeem-tokens";
 const Lottery: NextPage = () => {
     const [address, setAdress] = useState("");
     const debugMode = process.env.NEXT_PUBLIC_CHAIN_ENV == "hardhat"
-    let blockExplorer;
+    let blockExplorer = "";
     if (debugMode) blockExplorer = "http://localhost:3000/blockexplorer/transaction/"
     else blockExplorer = "https://sepolia.etherscan.io/tx/"
 
@@ -18,16 +18,19 @@ const Lottery: NextPage = () => {
         <>
             <div className="text-center mt-8 bg-secondary p-10">
                 <h1 className="text-4xl my-0">Lottery !</h1>
-                <p className="text-neutral">
-                    Lottery
-                    <br /> Check{" "}
+                <div >
+                    <p className="text-neutral">
+                        Lottery
+                        <br /> Check{" "}
+                    </p>
+
                     <LoadContractAddress setAddress={setAdress} address={address} />
                     {address && (<>
                         <BuyTokens address={address} blockExplorer={blockExplorer} />
                         <DelegateAllowance address={address} blockExplorer={blockExplorer} />
                         <RedeemTokens address={address} blockExplorer={blockExplorer} />
                     </>)}
-                </p>
+                </div>
             </div>
         </>
     );
